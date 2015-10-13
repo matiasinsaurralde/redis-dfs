45ef124a3c051bc7de9089272bc1658373e33122
Matias Insaurralde <matias@insaurral.de>
2015-10-13 17:54:23 -0300

    read hosts from official_test_hosts

--- test.js
+++ test.js
@@ -1,8 +1,11 @@
 var DRFS = require( './' ),
-    drfs = new DRFS();
+    drfs = new DRFS(),
+    fs = require( 'fs' );
 
+var hosts = fs.readFileSync( 'official_test_hosts' ).toString().split( "\n" );
+    hosts.pop(); // remove last/empty \n :o
 
-drfs.addHosts( [ 'x.x.x.x', 'x.x.x.x' ] );
+drfs.addHosts( hosts );
 
 drfs.put( 'JoeWilliams-StackODollars.mp3', function( files ) {
   console.log( '-> put ok!', files );
